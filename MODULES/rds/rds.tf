ef47e9577c6bc89eb184c3bdbb642c52296ac7cc
Aziza Rakhmatillaeva <166773287+arakhmatillaeva@users.noreply.github.com>
2024-05-28 15:08:29 -0500

    Update rds.tf

--- MODULES/rds/rds.tf
+++ MODULES/rds/rds.tf
@@ -1,17 +1,16 @@
-resource "aws_db_instance" "db" {
-  allocated_storage    = 10
-  db_name              = "ziyo"
-  engine               = "postgres"
-  engine_version       = "12.15"
-  instance_class       = "db.t3.micro"
-  username             = "aziza_user"
-  password             = aws_ssm_parameter.aziza.value
-  parameter_group_name = "db.postgres12"
-  skip_final_snapshot  = true
-  db_subnet_group_name = "ziyo_sub_group"
-  vpc_security_group_ids = ["sg-0215ec9cd2d2b3dd7"]
-  storage_type         = "gp2"
-  
+resource "aws_db_instance" "ziyo" {
+  allocated_storage      = var.allocated_storage
+  db_name                = var.ziyo_name
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  username               = var.username
+  password               = aws_ssm_parameter.aziza.value
+  parameter_group_name   = var.parameter_group_name
+  skip_final_snapshot    = var.skip_final_snapshot
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  storage_type           = var.storage_type
 }
 
 resource "aws_ssm_parameter" "aziza" {
